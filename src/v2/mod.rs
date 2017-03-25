a496e72a738389cae6cbcde2b011a8874b2ed3b7
Luca Bruno <lucab@debian.org>
2017-03-25 22:21:37 +0000

    lib: split tags to its own module

--- src/v2/mod.rs
+++ src/v2/mod.rs
@@ -16,6 +16,9 @@ pub use self::config::Config;
 mod manifest;
 pub use self::manifest::{Manifest, FutureManifest};
 
+mod tags;
+pub use self::tags::{Tags, FutureTags};
+
 /// A Client to make outgoing API requests to a registry.
 #[derive(Debug)]
 pub struct Client {
@@ -179,40 +182,6 @@ impl Client {
             .map_err(|e| e.into());
         return Ok(Box::new(fres));
     }
-
-    pub fn get_tags(&self, name: &str, limit: Option<u32>) -> Result<FutureTags> {
-        let url = {
-            let mut s = format!("{}/v2/{}/tags/list", self.base_url, name);
-            if let Some(n) = limit {
-                s = s + &format!("?n={}", n);
-            };
-            try!(hyper::Uri::from_str(s.as_str()))
-        };
-        let req = self.new_request(hyper::Method::Get, url);
-        let freq = self.hclient.request(req);
-        let fres = freq.map_err(|e| e.into())
-            .and_then(move |r| {
-                if r.status() != &hyper::status::StatusCode::Ok {
-                    return Err(hyper::Error::Status);
-                };
-                Ok(r)
-            })
-            .and_then(move |r| {
-                r.body().fold(Vec::new(), |mut v, chunk| {
-                    v.extend(&chunk[..]);
-                    futures::future::ok::<_, hyper::Error>(v)
-                })
-            })
-            .and_then(|chunks| {
-                let s = String::from_utf8(chunks).unwrap();
-                Ok(s)
-            })
-            .and_then(move |body| {
-                serde_json::from_slice(body.as_bytes()).map_err(|_| hyper::Error::Status)
-            })
-            .map_err(|e| e.into());
-        return Ok(Box::new(fres));
-    }
 }
 
 pub type FutureToken = Box<futures::Future<Item = TokenAuth, Error = Error>>;
@@ -228,13 +197,6 @@ pub type FutureCatalog = Box<futures::Future<Item = Catalog, Error = Error>>;
 #[derive(Debug,Default,Deserialize,Serialize)]
 pub struct Catalog {
     pub repositories: Vec<String>,
-}
-
-pub type FutureTags = Box<futures::Future<Item = Tags, Error = Error>>;
-#[derive(Debug,Default,Deserialize,Serialize)]
-pub struct Tags {
-    name: String,
-    tags: Vec<String>,
 }
 
 #[derive(Debug,Default,Deserialize,Serialize)]
---
+++ src/v2/tags.rs
@@ -0,0 +1,45 @@
+use v2::*;
+
+#[derive(Debug,Default,Deserialize,Serialize)]
+pub struct Tags {
+    name: String,
+    tags: Vec<String>,
+}
+pub type FutureTags = Box<futures::Future<Item = Tags, Error = Error>>;
+
+impl Client {
+    /// List existing tags for an image.
+    pub fn get_tags(&self, name: &str, limit: Option<u32>) -> Result<FutureTags> {
+        let url = {
+            let mut s = format!("{}/v2/{}/tags/list", self.base_url, name);
+            if let Some(n) = limit {
+                s = s + &format!("?n={}", n);
+            };
+            try!(hyper::Uri::from_str(s.as_str()))
+        };
+        let req = self.new_request(hyper::Method::Get, url);
+        let freq = self.hclient.request(req);
+        let fres = freq.map_err(|e| e.into())
+            .and_then(move |r| {
+                          if r.status() != &hyper::status::StatusCode::Ok {
+                              return Err(hyper::Error::Status);
+                          };
+                          Ok(r)
+                      })
+            .and_then(move |r| {
+                          r.body().fold(Vec::new(), |mut v, chunk| {
+                    v.extend(&chunk[..]);
+                    futures::future::ok::<_, hyper::Error>(v)
+                })
+                      })
+            .and_then(|chunks| {
+                          let s = String::from_utf8(chunks).unwrap();
+                          Ok(s)
+                      })
+            .and_then(move |body| {
+                          serde_json::from_slice(body.as_bytes()).map_err(|_| hyper::Error::Status)
+                      })
+            .map_err(|e| e.into());
+        return Ok(Box::new(fres));
+    }
+}

1b7a78e3b631c8f7ea6eab49809d1f1e87e734b0
Luca Bruno <lucab@debian.org>
2017-03-25 23:01:26 +0000

    configure: use custom user-agent

--- tests/base-client.rs
+++ tests/base-client.rs
@@ -9,10 +9,7 @@ static API_VERSION_K: &'static str = "Docker-Distribution-API-Version";
 static API_VERSION_V: &'static str = "registry/2.0";
 
 fn mock_version_ep() {
-    mock("GET", "/v2/")
-        .with_status(200)
-        .with_header(API_VERSION_K, API_VERSION_V)
-        .create();
+    mock("GET", "/v2/").with_status(200).with_header(API_VERSION_K, API_VERSION_V).create();
 }
 
 #[test]
@@ -58,6 +55,60 @@ fn test_base_useragent() {
 
     let res = tcore.run(futcheck).unwrap();
     assert_eq!(res, true);
+
+    mockito::reset();
+}
+
+#[test]
+fn test_base_custom_useragent() {
+    let ua = "custom-ua/1.0";
+
+    mock("GET", "/v2/")
+        .match_header("user-agent", ua)
+        .with_status(200)
+        .with_header(API_VERSION_K, API_VERSION_V)
+        .create();
+
+    let mut tcore = Core::new().unwrap();
+    let dclient = dkregistry::v2::Client::configure(&tcore.handle())
+        .registry(mockito::SERVER_ADDRESS)
+        .insecure_registry(true)
+        .user_agent(Some(ua.to_string()))
+        .username(None)
+        .password(None)
+        .build()
+        .unwrap();
+
+    let futcheck = dclient.is_v2_supported().unwrap();
+
+    let res = tcore.run(futcheck).unwrap();
+    assert_eq!(res, true);
+
+    mockito::reset();
+}
+
+#[test]
+fn test_base_no_useragent() {
+    mock("GET", "/v2/")
+        .match_header("user-agent", dkregistry::USER_AGENT)
+        .with_status(200)
+        .with_header(API_VERSION_K, API_VERSION_V)
+        .create();
+
+    let mut tcore = Core::new().unwrap();
+    let dclient = dkregistry::v2::Client::configure(&tcore.handle())
+        .registry(mockito::SERVER_ADDRESS)
+        .insecure_registry(true)
+        .user_agent(None)
+        .username(None)
+        .password(None)
+        .build()
+        .unwrap();
+
+    let futcheck = dclient.is_v2_supported().unwrap();
+
+    let res = tcore.run(futcheck).unwrap();
+    assert_eq!(res, false);
 
     mockito::reset();
 }

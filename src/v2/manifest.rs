22137a5a010b5b847952214fb965eeabca1260e6
Luca Bruno <lucab@debian.org>
2017-03-25 21:53:28 +0000

    v2: implement has_manifest

--- src/v2/manifest.rs
+++ src/v2/manifest.rs
@@ -10,10 +10,11 @@ pub struct Manifest {
 pub type FutureManifest = Box<futures::Future<Item = Manifest, Error = Error>>;
 
 impl Client {
-    //! Fetch an image manifest.
-    //!
-    //! The name and reference parameter identify the image.
-    //! The reference may be either a tag or digest.
+
+    /// Fetch an image manifest.
+    ///
+    /// The name and reference parameters identify the image.
+    /// The reference may be either a tag or digest.
     pub fn get_manifest(&self, name: &str, reference: &str) -> Result<FutureManifest> {
         let url = try!(hyper::Uri::from_str(&format!("{}/v2/{}/manifests/{}",
                                                      self.base_url.clone(),
@@ -41,6 +42,29 @@ impl Client {
             .and_then(move |body| {
                           serde_json::from_slice(body.as_bytes()).map_err(|_| hyper::Error::Status)
                       })
+            .map_err(|e| e.into());
+        return Ok(Box::new(fres));
+    }
+
+    /// Check if an image manifest exists.
+    ///
+    /// The name and reference parameters identify the image.
+    /// The reference may be either a tag or digest.
+    pub fn has_manifest(&self, name: &str, reference: &str) -> Result<FutureBool> {
+        let url = {
+            let ep = format!("{}/v2/{}/manifests/{}",
+                             self.base_url.clone(),
+                             name,
+                             reference);
+            try!(hyper::Uri::from_str(ep.as_str()))
+        };
+        let req = self.new_request(hyper::Method::Head, url);
+        let freq = self.hclient.request(req);
+        let fres = freq.and_then(|r| match r.status() {
+                                     &hyper::status::StatusCode::Ok => Ok(true),
+                                     &hyper::status::StatusCode::NotFound => Ok(false),
+                                     _ => Err(hyper::Error::Status),
+                                 })
             .map_err(|e| e.into());
         return Ok(Box::new(fres));
     }

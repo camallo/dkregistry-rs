aea207727a4b5f10bba908edbb6968f387d82f0d
Stefan Junker <mail@stefanjunker.de>
2020-01-17 16:30:51 +0100

    errors: remove top-level uses

--- src/errors.rs
+++ src/errors.rs
@@ -1,23 +1,17 @@
 //! Error chains, types and traits.
 
-use base64;
-use http;
-use regex;
-use reqwest;
-use std::{io, string};
-
 error_chain! {
     foreign_links {
         Base64Decode(base64::DecodeError);
         HeaderInvalid(http::header::InvalidHeaderValue);
         HeaderParse(http::header::ToStrError);
         Hyper(http::Error);
-        Io(io::Error);
+        Io(std::io::Error);
         Json(serde_json::Error);
         Regex(regex::Error);
         Reqwest(reqwest::Error);
         UriParse(http::uri::InvalidUri);
-        Utf8Parse(string::FromUtf8Error);
+        Utf8Parse(std::string::FromUtf8Error);
         StrumParse(strum::ParseError);
     }
 }

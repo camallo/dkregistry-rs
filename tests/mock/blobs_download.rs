24109b407e0365b536e15282666ba822c820c1e5
Vadim Rutkovsky <vrutkovs@redhat.com>
2022-02-09 14:29:54 +0100

    tests/mock: add get_blob_stream unit test

--- tests/mock/blobs_download.rs
+++ tests/mock/blobs_download.rs
@@ -6,6 +6,8 @@ extern crate tokio;
 use self::mockito::mock;
 use self::tokio::runtime::Runtime;
 use crate::mock::blobs_download::sha2::Digest;
+use futures::stream::StreamExt;
+use futures::FutureExt;
 
 type Fallible<T> = Result<T, Box<dyn std::error::Error>>;
 
@@ -128,6 +130,40 @@ fn get_blobs_fails_with_inconsistent_layer() -> Fallible<()> {
         return Err("expected get_blob to fail with an inconsistent blob".into());
     };
 
+    mockito::reset();
+    Ok(())
+}
+
+#[test]
+fn get_blobs_stream() -> Fallible<()> {
+    let addr = mockito::server_address().to_string();
+
+    let name = "my-repo/my-image";
+    let blob = b"hello";
+    let digest = format!("sha256:{:x}", sha2::Sha256::digest(blob));
+
+    let ep = format!("/v2/{}/blobs/{}", &name, &digest);
+    let _m = mock("GET", ep.as_str())
+        .with_status(200)
+        .with_body(blob)
+        .create();
+
+    let runtime = Runtime::new().unwrap();
+    let dclient = dkregistry::v2::Client::configure()
+        .registry(&addr)
+        .insecure_registry(true)
+        .username(None)
+        .password(None)
+        .build()
+        .unwrap();
+
+    let futcheck = dclient.get_blob_stream(&name, &digest);
+
+    let mut result_stream = runtime.block_on(futcheck)?;
+    let stream_output = result_stream.next().now_or_never();
+    let output = stream_output.unwrap_or_else(|| panic!("No stream output"));
+    let received_blob = output.unwrap_or_else(|| panic!("No blob data"))?;
+    assert_eq!(blob.to_vec(), received_blob);
     mockito::reset();
     Ok(())
 }
